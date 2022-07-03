8f5f9637bcf2f97ba0b1a3a65d21a95c637af327
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-07-03 19:01:41 +0900

    Laravel Sanctumの設定 #3

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'users/login', 'users/logout'],
 
     'allowed_methods' => ['*'],
 
