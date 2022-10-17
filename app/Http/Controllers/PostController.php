8a5ec019074fd16e814e5f40892de185bca5ba62
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-10-17 15:23:22 +0900

    記事一覧表示機能のためのルーティングとコントローラを実装 (#7)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,6 +7,12 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
+    public function index(Request $request)
+    {
+        $posts = Post::with('user')->orderBy('created_at', 'desc')->get();
+        return response()->json($posts);
+    }
+
     public function create(Request $request)
     {
         $request->validate([
